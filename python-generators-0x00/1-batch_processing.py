8086edb13c408b3e4eccff63c11bb66ce0f06f13
Moussa Mahamat Nassour <132145833+Moussassoss@users.noreply.github.com>
2025-09-07 14:17:45 +0200

    Update 1-batch_processing.py

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -10,13 +10,13 @@ def stream_users_in_batches(batch_size):
         rows = cursor.fetchall()
         if not rows:
             break
-        yield rows   # generator yields a batch
+        yield rows
         offset += batch_size
     cursor.close()
     connection.close()
 
 def batch_processing(batch_size):
-    """Generator that yields each user over age 25"""
+    """Generator that yields users over age 25"""
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user['age'] > 25:
