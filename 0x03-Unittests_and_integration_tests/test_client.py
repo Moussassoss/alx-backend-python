6927d5debbac9109240b9a3910990c0c3d23df0b
Moussa Mahamat Nassour <132145833+Moussassoss@users.noreply.github.com>
2025-09-18 15:32:54 +0200

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -16,11 +16,11 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch("client.get_json")
     def test_org(self, org_name, mock_get_json):
         """Test that GithubOrgClient.org returns correct value"""
-        # Set return value for get_json mock
         mock_get_json.return_value = {"login": org_name}
 
         client = GithubOrgClient(org_name)
-        result = client.org()
+        # Access property, do not call it
+        result = client.org
 
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}"
