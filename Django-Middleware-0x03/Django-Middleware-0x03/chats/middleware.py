f19a9ba60979317a8f602553721b8fafe564501a
Moussa Mahamat Nassour <132145833+Moussassoss@users.noreply.github.com>
2025-09-28 16:29:50 +0200

    Update middleware.py

--- Django-Middleware-0x03/Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/Django-Middleware-0x03/chats/middleware.py
@@ -1,19 +1,22 @@
-import logging
+# chats/middleware.py
 from datetime import datetime
+from django.http import HttpResponseForbidden
 
-logger = logging.getLogger(__name__)
-handler = logging.FileHandler("requests.log")
-formatter = logging.Formatter("%(message)s")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
-
-class RequestLoggingMiddleware:
+class RestrictAccessByTimeMiddleware:
+    """
+    Middleware to restrict access to the messaging app outside 6 AM - 9 PM.
+    """
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        user = request.user if request.user.is_authenticated else "Anonymous"
-        log_message = f"{datetime.now()} - User: {user} - Path: {request.path}"
-        logger.info(log_message)
-        return self.get_response(request)
+        # Get current server hour
+        current_hour = datetime.now().hour
+
+        # Deny access if outside 6 AM - 9 PM
+        if current_hour < 6 or current_hour >= 21:
+            return HttpResponseForbidden("Access to the messaging app is restricted between 9 PM and 6 AM.")
+
+        # Otherwise continue processing
+        response = self.get_response(request)
+        return response
