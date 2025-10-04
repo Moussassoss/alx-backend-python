2814ff1c3b43062578e165ff482e5d2bdc66af0f
Moussa Mahamat Nassour <132145833+Moussassoss@users.noreply.github.com>
2025-10-04 16:13:34 +0200

    Update signals.py

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,7 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_delete
 from django.dispatch import receiver
-from .models import Message, MessageHistory
+from django.contrib.auth.models import User
+from .models import Message, MessageHistory, Notification
 
 
 @receiver(pre_save, sender=Message)
@@ -19,7 +20,27 @@ def log_message_edit(sender, instance, **kwargs):
             # Save history
             MessageHistory.objects.create(
                 message=old_message,
-                old_content=old_message.content
+                old_content=old_message.content,
+                edited_by=instance.edited_by if hasattr(instance, "edited_by") else None
             )
             # Mark as edited
             instance.edited = True
+
+
+@receiver(post_delete, sender=User)
+def cleanup_user_related_data(sender, instance, **kwargs):
+    """
+    Clean up related data when a User is deleted.
+    - Deletes messages sent or received by the user
+    - Deletes notifications belonging to the user
+    - Deletes message histories where the user was the editor
+    """
+    # Delete messages (sender or receiver)
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+
+    # Delete notifications
+    Notification.objects.filter(user=instance).delete()
+
+    # Delete histories linked to this user as editor
+    MessageHistory.objects.filter(edited_by=instance).delete()
